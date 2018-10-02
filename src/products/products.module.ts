8356a94a42997f5930a516232b31967dccfcb7f4
PawelStyrna <p.styrna@gmail.com>
2018-10-02 21:05:06 +0200

    refactor to use entities

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,19 +1,14 @@
-import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { Routes, RouterModule } from "@angular/router";
-import { ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouterModule, Routes } from "@angular/router";
+import { EffectsModule } from "@ngrx/effects";
 import { StoreModule } from "@ngrx/store";
-import { reducers } from "./store";
-
-// components
 import * as fromComponents from "./components";
-
-// containers
 import * as fromContainers from "./containers";
-
-// services
 import * as fromServices from "./services";
+import { reducers, effects } from "./store";
 
 // routes
 export const ROUTES: Routes = [
@@ -37,7 +32,8 @@ export const ROUTES: Routes = [
     ReactiveFormsModule,
     HttpClientModule,
     RouterModule.forChild(ROUTES),
-    StoreModule.forFeature("products", reducers)
+    StoreModule.forFeature("products", reducers),
+    EffectsModule.forFeature(effects)
   ],
   providers: [...fromServices.services],
   declarations: [...fromContainers.containers, ...fromComponents.components],
--- src/products/store/reducers/pizzas.reducer.ts
+++ src/products/store/reducers/pizzas.reducer.ts
@@ -2,44 +2,13 @@ import { Pizza } from "../../models/pizza.model";
 import * as fromPizzas from "../actions/pizzas.action";
 
 export interface PizzaState {
-  data: Pizza[];
+  entities: { [id: number]: Pizza };
   loaded: boolean;
   loading: boolean;
 }
 
 export const initialState: PizzaState = {
-  data: [
-    {
-      name: "Blazin' Inferno",
-      toppings: [
-        {
-          id: 10,
-          name: "pepperoni"
-        },
-        {
-          id: 9,
-          name: "pepper"
-        },
-        {
-          id: 3,
-          name: "basil"
-        },
-        {
-          id: 4,
-          name: "chili"
-        },
-        {
-          id: 7,
-          name: "olive"
-        },
-        {
-          id: 2,
-          name: "bacon"
-        }
-      ],
-      id: 1
-    }
-  ],
+  entities: {},
   loaded: false,
   loading: false
 };
@@ -57,10 +26,23 @@ export function reducer(
     }
 
     case fromPizzas.LOAD_PIZZAS_SUCCESS: {
+      const pizzas = action.payload;
+      const entities = pizzas.reduce(
+        (entities: { [id: number]: Pizza }, pizza: Pizza) => {
+          return {
+            ...entities,
+            [pizza.id]: pizza
+          };
+        },
+        {
+          ...state.entities
+        }
+      );
       return {
         ...state,
         loading: false,
-        loaded: true
+        loaded: true,
+        entities
       };
     }
 
@@ -75,6 +57,6 @@ export function reducer(
   return state;
 }
 
+export const getPizzasEntities = (state: PizzaState) => state.entities;
 export const getPizzasLoading = (state: PizzaState) => state.loading;
 export const getPizzasLoaded = (state: PizzaState) => state.loaded;
-export const getPizzas = (state: PizzaState) => state.data;
